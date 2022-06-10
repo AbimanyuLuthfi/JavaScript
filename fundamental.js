abcfad7c6d37b2a526ea46492e1656f1ccc27882
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2022-06-10 18:55:20 +0700

    Add files via upload

---
+++ fundamental.js
@@ -0,0 +1,9 @@
+// Ini merupakan komentar satu baris
+/* Ini merupakan komentar lebih dari 
+satu baris, Kita bebas mengisi teks apapun
+pada komentar ini.
+*/
+
+// Perintah untuk mencetak pada outuput/layar
+console.log("Hello World");
+console.log("Assalamualaikum Dunia");

685afaa54ee16c40c52dd2232d5cc3c54a560cb0
AbimanyuLuthfi <abimanyu177@gmail.com>
2023-02-08 10:56:16 +0700

    update destructuring-array-object.js

--- struktur-data-js/destructuring-array-object.js
+++ struktur-data-js/destructuring-array-object.js
@@ -15,3 +15,15 @@ console.log(movieName, type, idr)
 // Jika hanya membutuhkan satu objek saja, maka kita dapat menyesuaikannya, seperti contoh dibawah ini
 const {duration} = movies
 console.log(duration)
+
+// Destructuring Assignment
+// Penggunaan Destructuring Assignment, yaitu dapat mengisialisasi nilai baru pada suatu nilai yang terdapat dalam array/object
+const biography = {
+    name:"Abimanyu",
+    age:"20"
+}
+
+// Penggunaan Destructuring Assignment dapat dilakukan seperti contoh dibawah ini
+let name = "Luthfi"
+let age = "19"
+console.log(name,age)

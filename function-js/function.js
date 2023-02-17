8272ecda96e939a1d3ed836dfabdf2309c552296
AbimanyuLuthfi <abimanyu177@gmail.com>
2023-02-17 14:06:29 +0700

    added function-js

---
+++ function-js/function.js
@@ -0,0 +1,13 @@
+/* Function berfungsi untuk menghasilkan output berdasarka input tertentu */
+// Dalam membuat function dibutuhkan nama function, parameter, dan argument
+
+// Contoh Void Fuction
+
+
+
+// Contoh pembuatan function dengan return value
+ function multiply(a,b){
+    return a*b
+ }
+
+ console.log(`${multiply(4,4)}`)
